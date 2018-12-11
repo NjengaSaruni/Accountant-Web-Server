4ea50a40c14ac16e91b147e56ec082757b860afc
Peter Njenga <njengasaruni@gmail.com>
2018-12-11 23:07:43 +0300

    Env file

--- config/settings.py
+++ config/settings.py
@@ -76,9 +76,18 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('AP_DB_NAME', ''),
+        'USER': os.environ.get('AP_DB_USER', ''),
+        'PASSWORD': os.environ.get('AP_DB_PASSWORD', ''),
+        'HOST': os.environ.get('AP_DB_HOST', ''),
+        'PORT': os.environ.get('AP_DB_PORT', ''),
     }
 }
 
