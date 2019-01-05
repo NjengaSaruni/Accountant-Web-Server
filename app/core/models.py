ade7e013c7959da1696561d48c0c6a6884a47aa0
Peter Njenga <njengasaruni@gmail.com>
2019-01-05 21:50:18 +0300

    Primary key is uuid

--- app/core/models.py
+++ app/core/models.py
@@ -1,8 +1,11 @@
+import uuid
+
 from django.conf import settings
 from django.db import models
 
 
 class AbstractBase(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     created_at = models.DateTimeField(auto_created=True)
     updated_at = models.DateTimeField(auto_now=True)
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL)
