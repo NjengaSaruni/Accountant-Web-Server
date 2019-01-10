031c8dcd2643992d97b7569568d0eded4e007cb0
Peter Njenga <njengasaruni@gmail.com>
2019-01-10 11:44:16 +0300

    Login with email

--- app/accounts/serializers.py
+++ app/accounts/serializers.py
@@ -5,4 +5,4 @@ from . import models
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.User
-        fields = ('email', 'username','id')
+        fields = ('email', 'username','id','first_name', 'last_name')
