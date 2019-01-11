ab8cf3f671a88e4b475c1a754957b1f29639e933
Peter Njenga <njengasaruni@gmail.com>
2019-01-11 10:03:58 +0300

    Add docker configuration Remove pkg-resources from requirements

--- config/settings.py
+++ config/settings.py
@@ -91,14 +91,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'ap',
+#         'USER': 'app',
+#         'PASSWORD': 'app',
+#         'HOST': 'localhost',
+#         'PORT': 5432,
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'ap',
         'USER': 'app',
         'PASSWORD': 'app',
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': 5432,
     }
 }
