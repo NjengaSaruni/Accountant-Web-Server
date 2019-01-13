55a953df7b5663280e7085ce454e372dffd80436
Peter Njenga <njengasaruni@gmail.com>
2019-01-13 10:20:30 +0300

    Views for the finance app

--- app/finance/views.py
+++ app/finance/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from app.finance.models import Account, Transaction, Tag
+from app.finance.serializers import AccountSerializer, TransactionSerializer, TagSerializer
+
+
+class AccountListCreateView(generics.ListCreateAPIView):
+    serializer_class = AccountSerializer
+    queryset = Account.objects.all()
+
+
+class AccountDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = AccountSerializer
+    queryset = Account.objects.all()
+
+
+class TransactionListCreateView(generics.ListCreateAPIView):
+    serializer_class = TransactionSerializer
+    queryset = Transaction.objects.all()
+
+
+class TransactionDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TransactionSerializer
+    queryset = Transaction.objects.all()
+
+
+class TagListCreateView(generics.ListCreateAPIView):
+    serializer_class = TagSerializer
+    queryset = Tag.objects.all()
+
+
+class TagDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TagSerializer
+    queryset = Tag.objects.all()
+
