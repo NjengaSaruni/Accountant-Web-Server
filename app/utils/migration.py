717373cb526cddf36624df24f1d9e33aefd699be
Peter Njenga <njengasaruni@gmail.com>
2019-02-17 18:44:44 +0300

    Remove unnecessary comments

--- app/utils/migration.py
+++ app/utils/migration.py
@@ -49,13 +49,5 @@ if __name__ == '__main__':
                 transaction.save()
 
                 print(transaction)
-            except Exception as ex:
-                print(ex)
+            except Exception:
                 pass
-                    #     if line_count == 0:
-        #         print(f'Column names are {", ".join(row)}')
-        #         line_count += 1
-        #     else:
-        #         print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-        #         line_count += 1
-        # print(f'Processed {line_count} lines.')
