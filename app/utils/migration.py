bf5547dada6d096250aa1d29210307ff004f4895
Peter Njenga <njengasaruni@gmail.com>
2019-04-02 14:35:25 +0300

    Undo settings

--- app/utils/migration.py
+++ app/utils/migration.py
@@ -1,30 +1,24 @@
-import os
 import csv
+import os
 
 import django
-from django.utils import timezone
-import csv
-import os
 import pytz
-
-import django
 from django.utils import timezone
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 django.setup()
 from app.accounts.models import User
-
 from app.finance.models import Tag, Transaction
 
 if __name__ == '__main__':
     user = User.objects.get(email='peternjengask@gmail.com')
-
+    print(os.getcwd())
     with open('transactions.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
         current_tz = timezone.get_current_timezone()
-        Transaction.objects.all().delete()
+        Transaction.objects.filter(created_by=user).delete()
         for row in csv_reader:
             try:
                 date = pytz.utc.localize(timezone.datetime.strptime(row[0], '%Y-%m-%d'))
