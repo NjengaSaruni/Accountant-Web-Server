5341286a4c5e86ebc98ae58db14a6e1f40d5a2fb
Peter Njenga <njengasaruni@gmail.com>
2019-04-10 22:21:25 +0300

    Tests for limits

--- tests/finance/test_views.py
+++ tests/finance/test_views.py
@@ -22,4 +22,23 @@ class FinanceViewsTestCase(BaseTestClass):
             self.transactions_url,
             content_type='application/json'
         )
-        self.assertEqual(response.status_code, status.HTTP_200_OK) 
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_limits(self):
+        token = self.authenticate()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+        response = self.client.post(
+            self.limits_url,
+            data=json.dumps(self.limit_data),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_get_limits(self):
+        token = self.authenticate()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+        response = self.client.get(
+            self.limits_url,
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
